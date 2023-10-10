c1141adf0b77950c87f2ec87f303fb1230f43c2d
surajsardar1 <147463360+surajsardar1@users.noreply.github.com>
2023-10-10 04:43:33 +0000

    detatype

---
+++ .devcontainer/01_tutorial/tutt1.js
@@ -0,0 +1,18 @@
+
+
+let age=34;
+console.log(age);
+console.log(typeof age );
+
+let pink="color";
+console.log(pink);
+
+//datatype
+//string="";
+//number= 12345;
+//bigint=2344n'
+//boolean=true/false;
+//undefine=undefine;
+//null=object;
+//symbol=value;
+//object=;
